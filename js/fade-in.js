0da3f093b643a98e6b02cdced68d011ce3352f31
Derrick Holleman <derrickholleman@gmail.com>
2021-05-19 00:36:19 -0500

    fade in animation on project page

---
+++ js/fade-in.js
@@ -0,0 +1,16 @@
+/* FADE IN RIGHT */
+$(document).ready(function() {
+    $(window).scroll(function(){
+        $('.fadein-right').each(function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).innerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window + 100 > bottom_of_object ){
+                /* two animations at the same time with different durations */
+                $(this).animate({'right':'0'}, 500);  
+                $(this).animate({'opacity' : '1'}, {duration: 900, queue: false}); 
+            }            
+        });     
+    });    
+});
