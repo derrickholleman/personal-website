9b6b241a18d5b91e9565a1ad75d7fae22acfcf74
Derrick Holleman <derrickholleman@gmail.com>
2021-05-19 13:45:04 -0500

    added fade in left transition

--- js/fade-in.js
+++ js/fade-in.js
@@ -14,3 +14,20 @@ $(document).ready(function() {
         });     
     });    
 });
+
+/* FADE IN LEFT */
+$(document).ready(function() {
+    $(window).scroll(function(){
+        $('.fadein-left').each(function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).innerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window + 150 > bottom_of_object ){
+                /* two animations at the same time with different durations */
+                $(this).animate({'left':'0'}, 600);  
+                $(this).animate({'opacity' : '1'}, {duration: 650, queue: false}); 
+            }            
+        });     
+    });    
+});
