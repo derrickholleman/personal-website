089756625079f2d703c52dc2a77a85523f3b5a2e
Derrick Holleman <derrickholleman@gmail.com>
2021-05-22 00:19:46 -0500

    better transition

--- js/fade-in.js
+++ js/fade-in.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             var bottom_of_object = $(this).offset().top + $(this).innerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if( bottom_of_window + 350 > bottom_of_object ){
+            if( bottom_of_window + 550 > bottom_of_object ){
                 /* two animations at the same time with different durations */
                 $(this).animate({'right':'0'}, 600);  
                 $(this).animate({'opacity' : '1'}, {duration: 100, queue: false}); 
@@ -23,7 +23,7 @@ $(document).ready(function() {
             var bottom_of_object = $(this).offset().top + $(this).innerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if( bottom_of_window + 350 > bottom_of_object ){
+            if( bottom_of_window + 550 > bottom_of_object ){
                 /* two animations at the same time with different durations */
                 $(this).animate({'left':'0'}, 600);  
                 $(this).animate({'opacity' : '1'}, {duration: 100, queue: false}); 
